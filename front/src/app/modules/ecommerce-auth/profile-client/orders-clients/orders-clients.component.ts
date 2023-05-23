9f8aa5c24c3d7d1cc4372f48e2a38f4d46a9e944
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-05-23 16:54:24 -0600

    created: orders-clients component

--- front/src/app/modules/ecommerce-auth/profile-client/orders-clients/orders-clients.component.ts
+++ front/src/app/modules/ecommerce-auth/profile-client/orders-clients/orders-clients.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orders-clients',
   templateUrl: './orders-clients.component.html',
-  styleUrls: ['./orders-clients.component.sass']
+  styleUrls: ['./orders-clients.component.scss']
 })
-export class OrdersClientsComponent {
+export class OrdersClientsComponent implements OnInit {
+  @Input() listOrders:any = [];
+  constructor() { }
 
+  ngOnInit(): void {
+
+    console.log(this.listOrders);
+  }
 }
