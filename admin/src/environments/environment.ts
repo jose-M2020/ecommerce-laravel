3a1c648b4c6b583731be9f7d7f14941eb79eee7c
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-05-23 17:06:19 -0600

    added: assests, environments, cdn

---
+++ admin/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  appVersion: 'v723demo1',
+  USERDATA_KEY: 'authf649fc9a5f55',
+  isMockEnabled: false,
+  apiUrl: 'api',
+  // apiUrl: 'mysite.com/api',
+  URL_BACKEND: 'http://api.website.site/',
+  URL_SERVICIOS: 'http://api.website.site/api',
+  URL_FROTEND: 'http://admin.website.site',
+};
