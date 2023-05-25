0fc7a538306614a087672eb38ab81f5d08d4c6a9
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-05-24 19:46:08 -0600

    created: orders-review-clients component

--- front/src/app/modules/ecommerce-auth/profile-client/orders-review-clients/orders-review-clients.component.ts
+++ front/src/app/modules/ecommerce-auth/profile-client/orders-review-clients/orders-review-clients.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orders-review-clients',
   templateUrl: './orders-review-clients.component.html',
-  styleUrls: ['./orders-review-clients.component.sass']
+  styleUrls: ['./orders-review-clients.component.scss']
 })
-export class OrdersReviewClientsComponent {
+export class OrdersReviewClientsComponent implements OnInit {
 
+  @Input() listReviews:any = [];
+  IS_FORM:any = false;
+  sale_detail:any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  changeView(sale_detail:any,value:any){
+    this.IS_FORM = value;
+    this.sale_detail = sale_detail;
+  }
+
+  addReview(value:any){
+    console.log(value);
+    this.IS_FORM = false;
+    let INDEX = this.listReviews.findIndex((item:any) => item.id == value.sale_detail_id);
+    this.listReviews[INDEX].review = value.review;
+  }
+  
+  backView(value:any){
+    this.IS_FORM = value;
+  }
 }
