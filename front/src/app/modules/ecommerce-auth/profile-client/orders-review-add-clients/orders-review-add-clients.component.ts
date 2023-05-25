98bf852e8ad80b3486b1124bdb6034948d83df30
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-05-24 19:38:17 -0600

    created: order review add component

--- front/src/app/modules/ecommerce-auth/profile-client/orders-review-add-clients/orders-review-add-clients.component.ts
+++ front/src/app/modules/ecommerce-auth/profile-client/orders-review-add-clients/orders-review-add-clients.component.ts
@@ -1,10 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ProfileClientService } from '../../_services/profile-client.service';
+
+declare function  alertDanger([]):any;
+declare function alertSuccess([]):any;
 
 @Component({
   selector: 'app-orders-review-add-clients',
   templateUrl: './orders-review-add-clients.component.html',
-  styleUrls: ['./orders-review-add-clients.component.sass']
+  styleUrls: ['./orders-review-add-clients.component.scss']
 })
-export class OrdersReviewAddClientsComponent {
+export class OrdersReviewAddClientsComponent implements OnInit {
+  @Input() IS_FORM:any = false;
+  @Input() sale_detail:any;
+  @Output() Review: EventEmitter<any> = new EventEmitter();
+  @Output() backView: EventEmitter<any> = new EventEmitter();
 
+  review_selected:any = null;
+  rating:any  = 0;
+  message:any = "";
+
+  constructor(
+    public _profileHomeService: ProfileClientService,
+  ) { }
+
+  ngOnInit(): void {
+    this.review_selected = this.sale_detail.review;
+    if(this.review_selected){
+      this.message = this.review_selected.message;
+      this.rating = this.review_selected.rating;
+    }
+  }
+
+  back(){
+    this.IS_FORM = !this.IS_FORM;
+    this.backView.emit(this.IS_FORM);
+  }
+
+  getRating(value:any){
+    this.rating = value;
+  }
+
+  save(){
+    if(!this.message){
+      alertDanger("NECESITAS INGRESAR UNA DESCRIPCIÓN ");
+      return;
+    }
+    if(!this.rating){
+      alertDanger("NECESITAS INGRESAR UN RATING");
+      return;
+    }
+
+    let data = {
+      product_id: this.sale_detail.product.id,
+      sale_detail_id: this.sale_detail.id,
+      message: this.message,
+      rating: this.rating,
+    };
+    this._profileHomeService.addReview(data).subscribe((resp:any) => {
+      console.log(resp);
+      this.Review.emit({review: resp.review,sale_detail_id: this.sale_detail.id});
+      alertSuccess("LOS CAMBIOS SE HAN REGISTRADO CON EXITO LA RESEÑA");
+    })
+  }
+
+  update(){
+    if(!this.message){
+      alertDanger("NECESITAS INGRESAR UNA DESCRIPCIÓN ");
+      return;
+    }
+    if(!this.rating){
+      alertDanger("NECESITAS INGRESAR UN RATING");
+      return;
+    }
+
+    let data = {
+      product_id: this.sale_detail.product.id,
+      sale_detail_id: this.sale_detail.id,
+      message: this.message,
+      rating: this.rating,
+    };
+    this._profileHomeService.updateReview(this.review_selected.id,data).subscribe((resp:any) => {
+      console.log(resp);
+      this.Review.emit({review: resp.review,sale_detail_id: this.sale_detail.id});
+      alertSuccess("LOS CAMBIOS SE HAN REGISTRADO CON EXITO");
+    })
+  }
 }
