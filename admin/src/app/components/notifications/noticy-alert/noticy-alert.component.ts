8ef00c30d0c0d4ebbef99ccdffb63e4b6118f3b2
Jose Manuel <josemanuel.silva.jmst@gmail.com>
2023-05-26 21:21:56 -0600

    added: dashboard, layout, noticy-alert compoenent, config

---
+++ admin/src/app/components/notifications/noticy-alert/noticy-alert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-noticy-alert',
+  templateUrl: './noticy-alert.component.html',
+  styleUrls: ['./noticy-alert.component.scss']
+})
+export class NoticyAlertComponent {
+
+}
